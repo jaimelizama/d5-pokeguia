5a57a54008bf187245e6cefd8dc068e3e1826056
Jaime Lizama R <jlizama@poptimize.cl>
2021-08-25 13:05:48 -0400

    Subiendo PokeGuia
    Para mostrar imágenes y skills

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,64 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <!-- Panel central -->
+    <img class="img-banner" alt="Vue logo" src="./assets/Pokemon.png" />
+    <h2 class="my-4">PokeGuía</h2>
+    <label class="mr-2">Nombre:</label>
+    <input type="text" placeholder="Ingrese un pokémon" v-model="pokemonName" />
+    <button @click="searchPokemon()">Buscar</button>
+    <!-- Imagen de Pokemon buscado -->
+    <div>
+      <img :src="pokemonImage" />
+    </div>
+    <div>
+      <h3>Movimientos</h3>
+      <p v-for="(pokemonMove, $index) in moves" :key="$index">
+        {{ pokemonMove.move.name }}
+      </p>
+      <h3>Habilidades</h3>
+      <p v-for="(pokemonAbility, $index) in abilities" :key="$index">
+        {{ pokemonAbility.ability.name }}
+      </p>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data: () => ({
+    pokemonName: 'pikachu',
+    pokemon: null
+  }),
+  methods: {
+    searchPokemon() {
+      console.log(this.pokemonName)
+      this.fetchPokemon()
+    },
+    fetchPokemon() {
+      fetch(`https://pokeapi.co/api/v2/pokemon/${this.pokemonName}`)
+        .then((response) => response.json())
+        .then((json) => (this.pokemon = json))
+    }
+  },
+  computed: {
+    pokemonImage() {
+      return (
+        this.pokemon &&
+        this.pokemon.sprites &&
+        this.pokemon.sprites.front_default
+      )
+    },
+    moves() {
+      return this.pokemon.moves
+    },
+    abilities() {
+      return this.pokemon.abilities
+    }
+  },
+
+  created() {
+    this.fetchPokemon()
   }
 }
 </script>
@@ -24,5 +71,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.img-banner {
+  height: 200px;
 }
 </style>
